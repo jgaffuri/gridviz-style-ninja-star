fe3fc46237616df0b22e662bd8da15cd08db94df
jgaffuri <jgaf@proton.me>
2023-07-07 10:37:43 +0200

    a

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,20 @@
 // dev
 const LiveReloadPlugin = require('webpack-livereload-plugin')
+//const meta = require('./package.json')
+//console.log(meta)
+//https://github.com/d3/d3-shape/blob/main/rollup.config.js
+//https://webpack.js.org/configuration/output/#outputglobalobject
+
+//const go = Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^gridviz-/.test(key)).map(key => ({[key]: "gridviz"})))
+//console.log(Object.keys(meta.dependencies || {}).filter(key => key.startsWith("gridviz")).map(key => ({[key]: "gridviz"})))
 
 module.exports = {
     mode: 'development',
     output: {
         filename: 'gridviz-style-ninja-star.js',
-        library: 'gviz_sty_ns',
+        library: 'gviz',
         libraryTarget: 'umd',
+        //globalObject: go
     },
     plugins: [new LiveReloadPlugin()],
     watch: true,
